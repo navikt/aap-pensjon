d169fc99bd77d9c5d84f04e0db472c0ba4181cb0
Niro Karunakaran <niro.karunakaran@nav.no>
2023-01-27 10:47:27 +0100

    inital commit
    Co-authored-by: Richard B <richard.borge@nav.no>

---
+++ app/test/pensjon/AppTest.kt
@@ -0,0 +1,13 @@
+package pensjon
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+
+class AppTest {
+    @Test
+    fun test(){
+        assertTrue(true)
+    }
+
+}
