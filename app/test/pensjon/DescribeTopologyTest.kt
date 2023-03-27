ee24597939c93e2944a1550a0cf50839d0bce4e1
Robin <robin.tordly@nav.no>
2023-03-27 13:26:35 +0200

    describe topology

--- app/test/pensjon/DescribeTopologyTest.kt
+++ app/test/pensjon/DescribeTopologyTest.kt
@@ -2,25 +2,25 @@ package pensjon
 
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import no.nav.aap.kafka.streams.v2.config.StreamsConfig
-import no.nav.aap.kafka.streams.v2.test.KStreamsMock
+import no.nav.aap.kafka.streams.v2.test.StreamsMock
+import no.nav.aap.ktor.client.AzureConfig
 
 import org.junit.jupiter.api.Test
 import pensjon.kafka.topology
 import java.io.File
+import java.net.URL
 
 
 class DescribeTopology {
 
     @Test
     fun mermaid() {
+        val topology = topology(PesysClient(AzureConfig(URL("http:mock.no"), "", "")))
 
-        val topology = topology()
-
-        val kafka = KStreamsMock()
+        val kafka = StreamsMock()
         kafka.connect(topology, StreamsConfig("", ""), SimpleMeterRegistry())
 
-        kafka.visulize().mermaid().generateSubDiagrams().forEachIndexed { index, mermaid ->
-            File("../docs/topology$index.mmd").apply { writeText(mermaid) }
-        }
+        val diagram = kafka.visulize().mermaid().generateDiagram()
+        File("../docs/topology.mmd").apply { writeText(diagram) }
     }
 }
