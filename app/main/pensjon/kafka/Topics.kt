6987b93312acb310c045c37dbc04770fd3058161
Robin <robin.tordly@nav.no>
2023-04-18 10:37:25 +0200

    topic prefix

--- app/main/pensjon/kafka/Topics.kt
+++ app/main/pensjon/kafka/Topics.kt
@@ -4,8 +4,6 @@ import no.nav.aap.kafka.streams.v2.Topic
 import no.nav.aap.kafka.streams.v2.serde.JsonSerde
 
 object Topics {
-
-    val kravbehandling = Topic("pen-kravbehandling-v1", JsonSerde.jackson<KravHendelse>())
-    val vedtaksbehandling = Topic("pen-vedtaksbehandling-v1", JsonSerde.jackson<VedtakHendelse>())
-
+    val kravbehandling = Topic("pensjon-saksbehandling.pen-kravbehandling-v1", JsonSerde.jackson<KravHendelse>())
+    val vedtaksbehandling = Topic("pensjon-saksbehandling.pen-vedtaksbehandling-v1", JsonSerde.jackson<VedtakHendelse>())
 }
